1f2ebfbda8e3c5a47754d0b06da2c51bd1447e73
Carter Duncan <dukeblue2017@gmail.com>
2017-10-01 16:10:27 -0500

    made progress on JSONstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,43 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log('start.', 'obj:', obj, 'type:', typeof obj, 'typewstr', typeof (obj + ''))
+
+  if (obj === null || obj === undefined) {
+  	console.log('null block');
+  	return obj + '';
+  };
+
+  if (Array.isArray(obj)) {
+  	console.log('array block', obj, 'length:', obj.length);
+  	if (obj.length === 0) {
+  	  return '[]';
+  	} else if (obj.length === 1) {
+  	  var ans = stringifyJSON(obj[0]) + ']';	
+  	} else {
+  		var x = obj[0];
+  		var y = obj.slice(1,obj.length);
+  	    var a = stringifyJSON(x);
+  	    var b = stringifyJSON(y);
+  	    if (y.length > 0) {
+  	    	var ans = a + ',' + b;
+  	    } else if (y.length === 1) {
+  	    	var ans = a + y[0];
+  	    }
+  	} return ('[' + ans);
+  };  
+
+
+  if (typeof (obj + '') === 'string') {
+    console.log('string block', obj);
+    var strobj = (obj + '');
+    if (strobj[0] === '[' && strobj[strobj.length-1] === ']') {
+    	return '[' + obj + ']';
+    } else if (typeof obj === 'string') {
+    	return '"' + obj + '"'
+    } else {
+  	    return obj + '';
+    }
+  };
+
 };
