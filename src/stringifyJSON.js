b66c43f5e28c20cd3e035d426cea7252974a878e
Carter Duncan <dukeblue2017@gmail.com>
2017-10-02 01:51:21 -0500

    followed video through completion of stringifiable objects on stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,43 +4,27 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  console.log('start.', 'obj:', obj, 'type:', typeof obj, 'typewstr', typeof (obj + ''))
+  if ( Array.isArray(obj) ) {
+  	var results = [];
+  	for (var i = 0; i < obj.length; i++) {
+  	  results.push( stringifyJSON(obj[i]) );
+  	}
+  	return '[' + results.join(',') + ']';
+  }
 
-  if (obj === null || obj === undefined) {
-  	console.log('null block');
-  	return obj + '';
-  };
+  if (obj && typeof obj === 'object') {
+  	var results = [];
 
-  if (Array.isArray(obj)) {
-  	console.log('array block', obj, 'length:', obj.length);
-  	if (obj.length === 0) {
-  	  return '[]';
-  	} else if (obj.length === 1) {
-  	  var ans = stringifyJSON(obj[0]) + ']';	
-  	} else {
-  		var x = obj[0];
-  		var y = obj.slice(1,obj.length);
-  	    var a = stringifyJSON(x);
-  	    var b = stringifyJSON(y);
-  	    if (y.length > 0) {
-  	    	var ans = a + ',' + b;
-  	    } else if (y.length === 1) {
-  	    	var ans = a + y[0];
-  	    }
-  	} return ('[' + ans);
-  };  
+  	for (var key in obj) {
+  	  results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]))
+  	}
 
+  	return '{' + results.join(',') + '}';
+  }
 
-  if (typeof (obj + '') === 'string') {
-    console.log('string block', obj);
-    var strobj = (obj + '');
-    if (strobj[0] === '[' && strobj[strobj.length-1] === ']') {
-    	return '[' + obj + ']';
-    } else if (typeof obj === 'string') {
-    	return '"' + obj + '"'
-    } else {
-  	    return obj + '';
-    }
-  };
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"'
+  }
 
+  return '' + obj;
 };
