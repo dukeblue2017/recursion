83c6a1369e771fc1f7fa6e1247c0e41c99d63136
Carter Duncan <dukeblue2017@gmail.com>
2017-10-01 19:10:29 -0500

    got objects mostly working

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,25 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  console.log(json)
+  if (json[0] === '[') {
+  	var rest = json.substring(1,json.length)
+  	var nextBracketIndex = rest.indexOf(']')
+  	var first = json.slice(1,nextBracketIndex+1)
+  	var list = first.split(', ');
+  	console.log(list)
+  }
+  if (json[0] === '{') {
+  	console.log(json)
+  	var rest = json.substring(1,json.length)
+  	var nextBracketIndex = rest.indexOf('}')
+  	var first = json.slice(1,nextBracketIndex+1)
+  	var list = first.split(', ');
+  	var object = {};
+  	for (var i = 0; i < list.length; i++) {
+  	  var entry = list[i].split(': ');
+  	  object[entry[0]] = entry[1];
+  	}
+  	console.log(object);
+  }
 };
